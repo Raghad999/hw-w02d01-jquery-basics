8a135a36b7887857db0704b2080ad05690ecd04d
Raghad <raghad@Raghads-MacBook-Pro.local>
2019-04-15 01:22:07 +0300

    submitting hw

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
 // turn on stop function 
+
+
+function turnOfflight(){
+  $('#stopLight').css('background-color','black');
+  $('#slowLight').css('background-color','black');
+  $('#goLight').css('background-color','black');
+}
 function turnOnStopLight() {
-    // change background color of stop light to red
-    // changes background color of green and yellow to black
+  // change background color of stop light to red
+  // changes background color of green and yellow to black
+  turnOfflight();
+  $('#stopLight').css('background-color','red');
+ 
 }
 
 // turn on slow function
 function turnOnSlowLight() { 
-  // change background color of slow light to yellow
-  // changes background color of red and green to black
+// change background color of slow light to yellow
+// changes background color of red and green to black
+//$('#stopLight').css('background-color','black');
+turnOfflight();
+$('#slowLight').css('background-color','yellow');
+
 }
 
 // turn on go function
 function turnOnGoLight() {
-    // change background color of go light to green
-    // changes background color of red and yellow to black
+  // change background color of go light to green
+  // changes background color of red and yellow to black
+ 
+  turnOfflight();
+  $('#goLight').css('background-color','green');
 }
 
+
 // set stop light click event
+$('#stopButton').on('click',turnOnStopLight);
+
 
 // set slow light click event
-
+$('#slowButton').on('click',turnOnSlowLight);
 // set go light click event
+$('#goButton').on('click',turnOnGoLight);
 
